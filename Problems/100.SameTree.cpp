23657436c3843b8efdf31be5595afaa1f0c73c5d
Felipe Cuadra Plaza <fcuadra@ucm.es>
2025-01-24 20:57:39 +0100

    Add problem 100

---
+++ Problems/100.SameTree.cpp
@@ -0,0 +1,84 @@
+#include <vector>
+
+using namespace std;
+
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+Example 1:
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+*/
+
+//Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q)
+{
+    if (p == nullptr && q == nullptr)
+    {
+        return true;
+    }
+    else if (p == nullptr || q == nullptr)
+    {
+        return false;
+    }
+    else if (p->val != q->val)
+    {
+        return false;
+    }
+
+
+    bool isRightEqual = isSameTree(p->right, q->right);
+    bool isLeftEqual = isSameTree(p->left, q->left);
+
+    return isRightEqual && isLeftEqual;
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    TreeNode* root1 = new TreeNode(1);
+    root1->left = new TreeNode(2);
+    root1->right = new TreeNode(3);
+    root1->left->left = new TreeNode(4);
+    root1->left->right = new TreeNode(5);
+
+
+    bool result = isSameTree(root, root1);
+
+    return 0;
+}
