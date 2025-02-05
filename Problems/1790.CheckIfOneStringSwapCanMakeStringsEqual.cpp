6f40386bd55f20f32737a0cc86f5a446bd5b749d
Felipe Cuadra Plaza <fcuadra@ucm.es>
2025-02-06 00:02:18 +0100

    Added problem 1790

---
+++ Problems/1790.CheckIfOneStringSwapCanMakeStringsEqual.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/*
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+
+Example 1:
+
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+
+Example 2:
+
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+
+Example 3:
+
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+
+
+Constraints:
+
+1 <= s1.length, s2.length <= 100
+s1.length == s2.length
+s1 and s2 consist of only lowercase English letters.
+ */
+
+bool areAlmostEqual(string s1, string s2)
+{
+	int stringSwaps = 0;
+
+	unordered_set<char> set1 = unordered_set<char>();
+	unordered_set<char> set2 = unordered_set<char>();
+
+	for (int i = 0; i < s1.size(); ++i)
+	{
+		if (s1[i] != s2[i])
+		{
+			++stringSwaps;
+			set1.insert(s1[i]);
+			set2.insert(s2[i]);
+		}
+	}
+
+	return  set1 == set2 && (stringSwaps == 0 || stringSwaps == 2);
+}
+
+int main()
+{
+	string s1 = "caa";
+	string s2 = "aaz";
+
+	bool result = areAlmostEqual(s1, s2);
+
+	return 0;
+}
