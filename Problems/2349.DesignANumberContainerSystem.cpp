21b54288b3c34639e330cad5b182544a6de6861b
Felipe Cuadra Plaza <fcuadra@ucm.es>
2025-02-08 22:09:57 +0100

    Added problem 2349

---
+++ Problems/2349.DesignANumberContainerSystem.cpp
@@ -0,0 +1,107 @@
+#include <map>
+#include <set>
+#include <unordered_map>
+
+using namespace std;
+
+/*
+Design a number container system that can do the following:
+
+Insert or Replace a number at the given index in the system.
+Return the smallest index for the given number in the system.
+Implement the NumberContainers class:
+
+NumberContainers() Initializes the number container system.
+void change(int index, int number) Fills the container at index with the number. If there is already a number at that index, replace it.
+int find(int number) Returns the smallest index for the given number, or -1 if there is no index that is filled by number in the system.
+
+
+Example 1:
+
+Input
+["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"]
+[[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]]
+Output
+[null, -1, null, null, null, null, 1, null, 2]
+
+Explanation
+NumberContainers nc = new NumberContainers();
+nc.find(10); // There is no index that is filled with number 10. Therefore, we return -1.
+nc.change(2, 10); // Your container at index 2 will be filled with number 10.
+nc.change(1, 10); // Your container at index 1 will be filled with number 10.
+nc.change(3, 10); // Your container at index 3 will be filled with number 10.
+nc.change(5, 10); // Your container at index 5 will be filled with number 10.
+nc.find(10); // Number 10 is at the indices 1, 2, 3, and 5. Since the smallest index that is filled with 10 is 1, we return 1.
+nc.change(1, 20); // Your container at index 1 will be filled with number 20. Note that index 1 was filled with 10 and then replaced with 20.
+nc.find(10); // Number 10 is at the indices 2, 3, and 5. The smallest index that is filled with 10 is 2. Therefore, we return 2.
+
+
+Constraints:
+
+1 <= index, number <= 109
+At most 105 calls will be made in total to change and find.
+ */
+
+class NumberContainers
+{
+public:
+    NumberContainers() :numberToIndex(unordered_map<int, set<int>>()), indexToNumber(unordered_map<int, int>())
+    {
+    }
+
+    void change(int index, int number)
+    {
+        if (indexToNumber.count(index))
+        {
+            int oldNumber = indexToNumber[index];
+
+            numberToIndex[oldNumber].erase(index);
+            if (numberToIndex[oldNumber].empty())
+            {
+                numberToIndex.erase(oldNumber);
+            }
+        }
+
+
+        indexToNumber[index] = number;
+
+        numberToIndex[number].insert(index);
+    }
+
+    int find(int number)
+    {
+
+        if (numberToIndex.count(number) && !numberToIndex[number].empty())
+        {
+            return *numberToIndex[number].begin();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+private:
+    unordered_map<int, set<int>> numberToIndex;
+
+    unordered_map<int, int> indexToNumber;
+
+};
+
+
+int main()
+{
+    NumberContainers* obj = new NumberContainers();
+    obj->change(2, 10);
+    obj->change(1, 10);
+    obj->change(3, 10);
+    obj->change(5, 10);
+    int param_2 = obj->find(10);
+    obj->change(1, 20);
+    param_2 = obj->find(10);
+
+
+    delete obj;
+
+    return 0;
+}
